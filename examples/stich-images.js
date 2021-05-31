83265aae9a2f466e5b7d008c81fc7d47ca64ec9a
catpea <corporate.miou@gmail.com>
2021-05-30 20:16:08 -0400

    New Release

--- examples/stich-images.js
+++ examples/stich-images.js
@@ -1,11 +1,12 @@
 #!/usr/bin/env -S node --trace-uncaught
 import { inspect } from 'util';
 import { cmd } from '../index.js';
+
 const { montage } = cmd;
 const files = ['image1.png', 'image2.png', 'image3.png'];
 const tile = 2;
 const filePath = '/tmp/test.jpg';
 const command = montage({'-background': '"#212529"'}, ...files, {'-geometry': '320x230', '-tile':`${tile}x`}, filePath);
 
-console.log(await command.string);
+console.log(await command.string); // montage -background "#212529" a.png b.png c.png -geometry 320x230 -tile 2x /tmp/test.jpg
 // await command.exe; // to execute;
